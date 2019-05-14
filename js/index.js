fb292a75feac95b01c15dafd1cd8205a9a06775e
alexisjcarr <acarr59@gmail.com>
2019-05-14 14:43:04 -0500

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+let navEls = document.querySelectorAll('nav .nav-link');
+
+Array.from(navEls);
+
+navEls.forEach(navEl => {
+    navEl.addEventListener('mouseover', event => {
+        event.target.style.color = '#90CAF9';
+        console.log(event)
+    });
+    navEl.addEventListener('mouseout', event => {
+        event.target.style.color = '#212529';
+        console.log(event)
+    });
+});
+
